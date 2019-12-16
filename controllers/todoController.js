18c7dedb4c44943c8cfee31a640930232cb5f3ee
Rawaa <rawaa.bouali@esen.tn>
2019-12-16 20:53:38 +0100

    WS complete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,74 @@
 const express = require('express');
-const {Todo}=require('./../models/todo');
+const { Todo } = require('./../models/todo');
 
 var app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send({message:'welcom to todo Controller'})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'welcom to todo Controller' })
 })
 
-app.post('/add',(req,res)=>{
-    let data =req.body;
-    let todo=new Todo({
-        description:data._description,
-        idUser:data._idUser
+app.post('/add', (req, res) => {
+    let data = req.body;
+    let todo = new Todo({
+        description: data._description,
+        idUser: data._idUser
     })
-   todo.save().then((todoFromdb)=>{
+    todo.save().then((todoFromdb) => {
         console.log(todoFromdb);
-        res.status(200).send({message:"todo ajouté avec succés"})
-    
-    }).catch((erreur)=>{
+        res.status(200).send({ message: "todo ajouté avec succés" })
+
+    }).catch((erreur) => {
         res.status(400).send(erreur)
     })
 
 })
 
-module.exports=app
+app.get('/list/:idUser', (req, res) => {
+    let idUser = req.params.idUser
+    Todo.find({ idUser: idUser }).then((todos) => {
+        console.log(todos)
+        let todoList = [];
+        let doneList = [];
+        for (let i = 0; i < todos.length; i++) {
+            if (!todos[i].etat) {
+                todoList.push(todos[i]);
+            }
+            else{
+                doneList.push(todos[i]);
+            }
+        }
+        res.status(200).send({todoList,doneList})
+    }).catch((erreur) => {
+        res.status(400).send(erreur)
+    })
+})
+
+app.put('/done',(req,res)=>{
+    let idTodo = req.body._id
+    Todo.findByIdAndUpdate({_id:idTodo},{etat:true,dateFin:new Date()},{new:true}).then((todo)=>{
+        res.status(200).send({message:"todo terminer avec succées"})
+    }).catch((erreur)=>{
+        res.status(400).send(erreur);
+        
+    })
+})
+app.put('/update',(req,res)=>{
+    let description = req.body._description;
+    let idTodo = req.body._id;
+    Todo.findByIdAndUpdate({_id:idTodo},{description},{new:true}).then((todo)=>{
+        res.status(200).send({message:"todo modifié avec succées"})
+    }).catch((erreur)=>{
+        res.status(400).send(erreur);
+        
+    })
+})
+app.delete('/delete',(req,res)=>{
+    let idTodo = req.body._id;
+    Todo.findByIdAndDelete({_id:idTodo}).then((todo)=>{
+        res.status(200).send({message:"todo supprimé avec succées"})
+    }).catch((erreur)=>{
+        res.status(400).send(erreur);
+        
+    })
+})
+module.exports = app
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,49 @@
 const express = require('express');
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
-const {User}=require('./../models/user');
-const {mongoose}=require('./../db/config');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('./../models/user');
+const { mongoose } = require('./../db/config');
 var app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send({message:'welcom to user Controller'})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'welcom to user Controller' })
 })
 
-app.post('/register',(req,res)=>{
-    let data =req.body;
-    let user=new User({
-        firstname:data._firstname,
-        lastname:data._lastname,
-        phone:data._phone,
-        email:data._email,
-        password:bcrypt.hashSync(data._password,10)
+app.post('/register', (req, res) => {
+    let data = req.body;
+    let user = new User({
+        firstname: data._firstname,
+        lastname: data._lastname,
+        phone: data._phone,
+        email: data._email,
+        password: bcrypt.hashSync(data._password, 10)
     })
-    user.save().then((userFromdb)=>{
+    user.save().then((userFromdb) => {
         console.log(userFromdb);
-        res.status(200).send({message:"utilisateur enregistré avec succés"})
-    
-    }).catch((erreur)=>{
+        res.status(200).send({ message: "utilisateur enregistré avec succés" })
+
+    }).catch((erreur) => {
         res.status(400).send(erreur)
     })
 
 })
-app.post('/login',(req,res)=>{
-let data =req.body;
-let email=data._email;
-let password=data._password;
-User.findOne({email}).then((userFromdb)=>{
-    if (!userFromdb){res.status(404).send({message:"email incorrect"})}
-    else {
-    let compare=bcrypt.compareSync(password,userFromdb.password)
-    if (!compare){res.status(404).send({message:"password incorrect"})}
-    else {let token =jwt.sign({idUsesr:userFromdb._id},'my secret key')
-    res.status(200).send({token})
-    }
-}
-}).catch((error)=>{
-    console.log(error);
-    
+app.post('/login', (req, res) => {
+    let data = req.body;
+    let email = data._email;
+    let password = data._password;
+    User.findOne({ email }).then((userFromdb) => {
+        if (!userFromdb) { res.status(404).send({ message: "email incorrect" }) }
+        else {
+            let compare = bcrypt.compareSync(password, userFromdb.password)
+            if (!compare) { res.status(404).send({ message: "password incorrect" }) }
+            else {
+                let token = jwt.sign({ idUser: userFromdb._id }, 'my secret key')
+                res.status(200).send({ token })
+            }
+        }
+    }).catch((error) => {
+        console.log(error);
+
+    })
 })
-})
-module.exports=app
+module.exports = app
